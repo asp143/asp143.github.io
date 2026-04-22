38bea664deef976ec870094e750bc786cbc9cbdf
Ralph Jonas D. Mungcal <mungcs09@gmail.com>
2026-04-22 19:00:49 +0800

    Show full blog publication dates (#58)

--- src/utils/blog.ts
+++ src/utils/blog.ts
@@ -10,7 +10,11 @@ export async function getPublishedPosts(): Promise<BlogEntry[]> {
 }
 
 export function formatPubDate(date: Date): string {
-  return `${date.getFullYear()}.${String(date.getMonth() + 1).padStart(2, '0')}`;
+  const year = date.getUTCFullYear();
+  const month = String(date.getUTCMonth() + 1).padStart(2, '0');
+  const day = String(date.getUTCDate()).padStart(2, '0');
+
+  return `${year}.${month}.${day}`;
 }
 
 export function formatPubDateLong(date: Date): string {
